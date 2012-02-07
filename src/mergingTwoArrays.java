a3abadf98e3bfeab802d2610467dd7926b882608
Matthew K. Chan <mattkc7@gmail.com>
2012-02-07 13:05:49 -0800

    adding problem 2 on merging arrays

---
+++ src/mergingTwoArrays.java
@@ -0,0 +1,29 @@
+
+public class mergingTwoArrays {
+
+	/**
+	 * READ ME
+	 * yoo are given two sorted arrays, A and B, where A has a large
+	 * enough buffer at the end to hold B. Complete the function
+	 * 'merge' to merge B into A in sorted order.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[7];
+		a[0] = 2;
+		a[1] = 4;
+		a[2] = 6;
+		a[3] = 8;
+		
+		int[] b = {1, 3, 5};
+	}
+	
+	public static void merge(int[] a, int[] b, int lastA, int lastB){
+		int indexA = lastA - 1; //index of last element in a
+		int indexB = lastB - 1; //index of last element in b
+		int indexMerged = lastB + lastA - 1;
+		
+		/* merge a and b */
+	}
+
+}
